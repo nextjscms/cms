7b5e90eded97e84bf8db328685825ae6dfc72fac
Lem Soprom <soprom.lem@gmail.com>
2026-07-30 23:30:17 +0700

    Remove test files containing credentials

--- packages/core/test-action.js
+++
@@ -1,8 +0,0 @@
-const { NeonAdapter } = require('@nextjscms/adapter-neon');
-const adapter = new NeonAdapter();
-async function run() {
-  console.log("Testing connection...");
-  const res = await adapter.testConnection("postgresql://mockuser:mockpass@ep-mock-neon.mock.tech/neondb?sslmode=require");
-  console.log("Result:", res);
-}
-run().catch(console.error);
